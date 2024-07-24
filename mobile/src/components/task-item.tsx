9a3063c227a6246ea8fcc96b8ef79325fef76a33
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-07-24 15:07:43 -0300

    chore: Update TaskItem component styling and layout

--- mobile/src/components/task-item.tsx
+++ mobile/src/components/task-item.tsx
@@ -14,7 +14,7 @@ const TaskItem = ({
   props
 }: TaskItemProps) => {
   return (
-    <View className="w-full py-6 px-5 bg-white rounded-2xl shadow-md">
+    <View className="w-full py-6 px-5 bg-white rounded-2xl shadow-sm mb-3">
       <Text className="text-sm text-gray-400">{props.time}</Text>
       <Text className="text-xl font-semibold">{props.title}</Text>
       <Text className="text-sm">{props.description}</Text>
